49accc1732f59e86611038832e8eb6e4b89a8c91
pjw <177563971@163.com>
2017-08-07 10:19:56 +0800

    首页已经设置了cell样式

--- DYZB/DYZB/Home/Controller/RecommendViewController.swift
+++ DYZB/DYZB/Home/Controller/RecommendViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 private let kItemMargin : CGFloat = 10
 private let kItemW = (kScreenW - 3 * kItemMargin) / 2
-private let kItemH = kItemW * 3 / 4
+private let kNormalItemH = kItemW * 3 / 4
+private let kPrettyItemH = kItemW * 4 / 3
+
+
 private let kHeaderViewH : CGFloat = 50
 
 private let kNormalCellID = "kNormalCellID"
 private let kHeaderViewID = "kHeaderViewID"
+private let kPrettyCellID = "kPrettyCellID"
+
 
 
 
@@ -26,7 +31,7 @@ class RecommendViewController: UIViewController {
         
         //1.创建布局
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize.init(width: kItemW, height: kItemH)
+        layout.itemSize = CGSize.init(width: kItemW, height: kNormalItemH)
         layout.minimumLineSpacing = 0
         layout.minimumInteritemSpacing = kItemMargin
         layout.headerReferenceSize = CGSize.init(width: kScreenW, height: kHeaderViewH)
@@ -36,8 +41,15 @@ class RecommendViewController: UIViewController {
         let collectionView = UICollectionView.init(frame: CGRect.init(x: 0, y: 0, width: kScreenW, height: kScreenH), collectionViewLayout: layout)
         collectionView.register(UINib.init(nibName: "CollectionHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: kHeaderViewID)
         collectionView.register(UINib.init(nibName: "CollectionNormalCell", bundle: nil), forCellWithReuseIdentifier: kNormalCellID)
+        collectionView.register(UINib.init(nibName: "CollectionPrettyCell", bundle: nil), forCellWithReuseIdentifier: kPrettyCellID)
+        
+        
+        
+        
+        
         collectionView.backgroundColor = UIColor.white
         collectionView.dataSource = self
+        collectionView.delegate = self
         collectionView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
         
         return collectionView
@@ -74,7 +86,7 @@ extension RecommendViewController{
 
 
 //MARK: - 遵守collectionview代理方法
-extension RecommendViewController : UICollectionViewDataSource {
+extension RecommendViewController : UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         
@@ -93,7 +105,19 @@ extension RecommendViewController : UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
+        //1.定义cell
+        var cell : UICollectionViewCell!
+        
+        if indexPath.section == 1 {
+            
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: kPrettyCellID, for: indexPath)
+            
+        }else {
+        
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
+
+        }
+        
         
         
         return cell
@@ -109,6 +133,19 @@ extension RecommendViewController : UICollectionViewDataSource {
     }
     
     
+    // MARK: - 对不同section中cell高度的设置, 动态设置
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if indexPath.section == 1 {
+            
+            return CGSize.init(width: kItemW, height: kPrettyItemH)
+            
+        }else {
+        
+            return CGSize.init(width: kItemW, height: kNormalItemH)
+        }
+        
+    }
     
     
     
---
+++ DYZB/DYZB/Main/View/CollectionPrettyCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionPrettyCell.swift
+//  DYZB
+//
+//  Created by MacMini on 2017/8/7.
+//  Copyright © 2017年 MacMini. All rights reserved.
+//
+
+import UIKit
+
+class CollectionPrettyCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
